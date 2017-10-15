f90536297c053e089d97f00f4b7c4c9da3508f04
hiepxanh <hiepxanh@gmail.com>
2017-10-15 10:56:20 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCmcuuqCKCVJ6H8UHeiX5WNF1f9dnzfR24",
-    authDomain: "quickstart-9ddcf.firebaseapp.com",
-    databaseURL: "https://quickstart-9ddcf.firebaseio.com",
-    projectId: "quickstart-9ddcf",
-    storageBucket: "gs://quickstart-9ddcf.appspot.com",
-    messagingSenderId: "314021235019"
+
   }
 };
